c5e4a01765e942c5cd8bb2183066ddd7ab249e62
hugo-friederich <hugo.friederich@ens2m.org>
2024-11-13 13:54:31 +0100

    add tools

--- src/test/java/DatabaseConnectionManagerTest.java
+++ src/test/java/DatabaseConnectionManagerTest.java
@@ -2,7 +2,10 @@ import org.junit.Test;
 import org.trash_hunter.DatabaseConnectionManager;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -10,9 +13,8 @@ public class DatabaseConnectionManagerTest {
 
     @Test
     public void shouldConnectToDatabase() throws SQLException {
-
-        DatabaseConnectionManager connectionManager = new DatabaseConnectionManager("nemrod.ens2m.fr", "2024-2025_s1_vs1_tp2_Trash_Hunter", "student", "YTDTvj9TR3CDYCmP");
-        Connection connection = connectionManager.getConnection();
-        assertNotNull(connection);
+        
+        Connection connexion = DriverManager.getConnection("jdbc:mariadb://nemrod.ens2m.fr:3306/tp_jdbc", "etudiant", "YTDTvj9TR3CDYCmP");
+      
     }
 }

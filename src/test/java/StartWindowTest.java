c9b33d4fad70e348c508dad86105f3550d2311ca
hugo-friederich <hugo.friederich@ens2m.org>
2024-11-24 16:49:33 +0100

    solve panels problems #23

---
+++ src/test/java/StartWindowTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import org.trash_hunter.windows.Start_window;
+
+import java.sql.SQLException;
+
+public class StartWindowTest {
+    @Test
+    public void shouldOpenStartWindow () throws SQLException {
+        Start_window startWindow=new Start_window();
+    }
+}
